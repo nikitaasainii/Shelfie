c5436d397e08c16c120cc42f6888f9e0c2ad44a1
Nikita saini <nikitasaini12@gmail.com>
2026-05-18 15:01:09 +0530

    login & register pages added

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,29 @@
+import {useNavigate} from 'react-router-dom'
 function RegisterPage(){
+        const navigate=useNavigate()
     return(
-        <div className="text-3xl text-pink-200">register</div>
+        <div className="relative flex flex-col h-screen bg-[#EDECED] items-center justify-center gap-3 ">
+            <img src="/icons.png" className="absolute top-0 left-0 w-64 h-auto" />
+            <img src="/bow.png" className="h-50 w-35 -mt-50"></img>
+            <h1 className="text-black font-['Times_New_Roman_MT_Condensed',_serif] text-8xl -mt-20 hover:scale-105 hover:text-[#FF1FA9] active:scale-95 transition-all duration-200 ">shelfie</h1>
+            <img src="/icon_design.png" className="absolute bottom-0 right-0 w-64 h-auto" />
+            <div className="flex flex-col gap-12 items-center">
+            <p className="text-[#FF1FA9] font-['Times_New_Roman_MT_Condensed',_serif] text-4xl italic">your very own digi-bookshelf</p>
+            <div className="bg-white rounded-3xl p-6 h-80 w-100 flex flex-col gap-2">
+                <label htmlFor="" className="font-['Times_New_Roman_MT_Condensed',_serif] text-[#FF1FA9] text-2xl">Name</label>
+                <input type="text"  className="border-1 rounded-full w-full outline-none px-4 py-2"></input>
+                <label htmlFor="" className="font-['Times_New_Roman_MT_Condensed',_serif] text-[#FF1FA9] text-2xl">Email</label>
+                <input type="text"  className="border-1 rounded-full w-full outline-none px-4 py-2"></input>
+                <label htmlFor="" className="font-['Times_New_Roman_MT_Condensed',_serif] text-[#FF1FA9] text-2xl">Password</label>
+                <input type="password"  className="border-1 rounded-full w-full outline-none px-4 py-2"></input>
+                
+                
+            </div>
+            <button className="bg-white text-[#FF1FA9] rounded-full border-1 border-[#FF1FA9] w-60 h-15 hover:scale-105 hover:bg-[#FF1FA9] hover:text-white active:scale-95 transition-all duration-200" onClick={()=>navigate('/shelf')}>Register</button>
+            </div>
+            
+
+        </div>
     )
 }
 export default RegisterPage
