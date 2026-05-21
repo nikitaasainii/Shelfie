fe9e15a7e418293678e8ede8f4dbb1a9a1907aea
Nikita saini <nikitasaini12@gmail.com>
2026-05-22 01:54:53 +0530

    landing page updated and pink color shade changed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,8 +24,8 @@ function HomePage(){
 
     return(
         <div className="relative flex flex-col h-screen bg-[#EDECED]">
-        <img src="../public/plant.png" className="absolute bottom-9 -left-30 w-120 z-15 h-75"/>
-        <img src="../public/plant.png" className="absolute bottom-9 -right-30 w-120 z-20 h-75" />
+        <img src="../public/plant.png" className="absolute bottom-2 -left-30 w-120 z-15 h-75"/>
+        <img src="../public/plant.png" className="absolute bottom-2 -right-30 w-120 z-20 h-75" />
         <div className="flex flex-col h-screen">
             <div className="h-1/2 bg-[#EDECED]">
             <div className="relative">
@@ -38,12 +38,12 @@ function HomePage(){
             </div>
             <div className="h-1/2 bg-[#EDECED] flex flex-col justify-center items-center px-20">
             <div className=" font-['Times_New_Roman',_serif] text-black text-7xl">
-                <h1>Hello <span className="italic text-[#FF1FA9]">Nikita!</span></h1>
-            <div className="absolute bottom-46 left-1/2 -translate-x-1/2 w-[40%] h-7 bg-black/100 blur-2xl rounded-[100%] z-0"></div>
+                <h1>Welcome to your <span className="italic text-[#F48AAE]">Shelf!</span></h1>
+
             </div>
             </div>
-            <div className="bottom-0 bg-[#FF1FA9] h-25 w-full whitespace-nowrap">
-            <p className="animate-marquee text-white font-['Times_New_Roman',_serif] text-2xl text-center mt-6">~Grab a coffee and your favorite book   -   Reading is dreaming with open eyes   -   Books make great gifts because they have whole worlds inside of them   -   Books are a uniquely portable magic~</p>
+            <div className="bottom-0 bg-[#F48AAE] h-10 w-full whitespace-nowrap">
+            <p className="animate-marquee text-white font-['Times_New_Roman',_serif] text-2xl text-center ">~Grab a coffee and your favorite book   -   Reading is dreaming with open eyes   -   Books make great gifts because they have whole worlds inside of them   -   Books are a uniquely portable magic~</p>
             </div>
 
         </div>
